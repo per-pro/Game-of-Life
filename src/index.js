55a39a317c620e669be57d960ce0cee906f7cd11
Itai Farhi <itaimf@gmail.com>
2021-08-27 18:02:26 -0700

    comments

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ window.onload = () => {
     'color');
     let colorInput = document.getElementById('color');
     colorInput.addEventListener('click', (e) => {
-        switch statement
-        switch(color) {
-            // case "Cold":
-            // case "Warm":
-            case "Dark":
-            case "Light":
+        // switch statement
+        // switch(color) {
+        //     // case "Cold":
+        //     // case "Warm":
+        //     case "Dark":
+        //     case "Light":
 
-        };
+        // };
     });
     let columnInput = document.getElementById('column');
     columnInput.addEventListener('click', (e) => {
